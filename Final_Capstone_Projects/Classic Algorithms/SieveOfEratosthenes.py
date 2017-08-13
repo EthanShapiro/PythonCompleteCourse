54c7929651f8268f78110190d93026a15db8d96d
Ethan Shapiro <23350520+EthanShapiro@users.noreply.github.com>
2017-08-13 15:11:58 -0700

    Added Sieve of Eratosthenes
    Signed-off-by: Ethan Shapiro <23350520+EthanShapiro@users.noreply.github.com>

--- Final_Capstone_Projects/Classic Algorithms/SieveOfEratosthenes.py
+++ Final_Capstone_Projects/Classic Algorithms/SieveOfEratosthenes.py
@@ -1,2 +1,36 @@
-# Find all prime numbers within a given range
+# Ancient algorithm to get all prime numbers within
+# up to a given number
 
+def get_primes(limit):
+    # create generator of numbers to save space
+    # NOTE 2 is first prime so start there
+    nums = (x for x in range(2, limit))
+
+    # Create a composite list
+    composite = []
+
+    # loop through numbers and add composite to
+    # new list
+    for p in nums:
+        if p in composite:
+            continue
+        # temp num to increase
+        num = p
+        # add all numbers up limit that are composite
+        while num < limit:
+            num += p
+            if num not in composite:
+                composite.append(num)
+    # Create primes
+    primes = []
+
+    # get all numbers not in composite
+    for num in range(2, limit):
+        if num not in composite:
+            primes.append(num)
+
+    return primes
+
+primes = get_primes(30)
+
+print(primes)
